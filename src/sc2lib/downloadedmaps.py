d6b141cb4315fdbd04b436e30f47c26920b1d5d2
Alex <alexander.komischke@stud.uni-hannover.de>
2018-08-15 16:37:41 +0200

    Added downloadedmaps.py file.

---
+++ src/sc2lib/downloadedmaps.py
@@ -0,0 +1,7 @@
+class DownloadedMaps:
+    Catalyst = "(2)CatalystLE"
+    AcidPlant = "(2)AcidPlantLE"
+    LostandFound = "(2)LostandFoundLE"
+    Redshift = "(2)RedshiftLE"
+    AbyssalReef = "AbyssalReefLE"
+    Blackpink = "BlackpinkLE"
